e2949102159b0f7a1b72170e6417ad2ce94d39cd
Kim <k.ds.diaz211@gmail.com>
2023-02-21 16:53:52 +0800

    Installed Json-server and made a database

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('./database/db.json');
+const middleware = jsonServer.defaults();
+const port = process.env.PORT || 8000;
 
+server.use(middleware);
+server.use(router);
+server.listen(port, ()=>{
+    console.log('Json Server is running')
+})
